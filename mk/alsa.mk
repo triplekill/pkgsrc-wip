dbc8ad9881770802b44c517c613440355f36581e
othyro <othyro>
2012-11-07 03:07:09 +0000

    wip/alsa-lib -> audio/alsa-lib

--- mk/alsa.mk
+++ mk/alsa.mk
@@ -1,4 +1,4 @@
-# $Id: rcd.mk,v 1.2 2004/08/27 07:53:09 bubuchka Exp $
+# $Id: alsa.mk,v 1.1 2008/01/16 19:43:56 ondratu Exp $
 # from "Id: oshavercd.mk,v 1.4 2003/11/19 21:28:47 bubuchka Exp"
 #
 #
@@ -31,7 +31,7 @@ buildlink-alsa:
 
 .	else
 OS_HAVE_ALSA=		NO
-.include "../../wip/alsa-lib/buildlink3.mk"
+.include "../../audio/alsa-lib/buildlink3.mk"
 .	endif
 .endif
 
